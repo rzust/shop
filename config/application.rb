cae0639461c2b8daa5062e74d7a8ff686511a53d
rzust <renatozust@gmail.com>
2014-12-05 17:15:01 -0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Shop
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
-    config.assets.precompile += %w( about/**/* blog/**/* brands/**/* catalog/**/* categories/**/* cs-page/**/* features/**/* forms/**/* gallery-widget/**/* hero/**/* info-blocks/**/* masterslider/**/* media/**/* offers/**/* overlays/**/* payment/**/* people/**/* posts-widgets/**/* )
+    config.assets.precompile += %w( about/* blog/* brands/* catalog/* categories/* cs-page/* features/* forms/* gallery-widget/* hero/* info-blocks/* masterslider/* media/* offers/* overlays/* payment/* people/* posts-widgets/* )
   end
 end
