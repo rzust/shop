64821858a367ac3b45776e4fc829736595ee724a
rzust <renatozust@gmail.com>
2014-12-08 18:46:46 -0500

    fix

--- lib/middleware2.rb
+++ lib/middleware2.rb
@@ -9,14 +9,14 @@ class Middleware2
     puts "calling middleware"
     if env['PATH_INFO'] =~ /\A\/attachment\/([a-f0-9\-]{36})\/(.*)/
       if attachment = Nifty::Attachments::Attachment.find_by_token($1)
-        # [200, {
-        #   'Content-Length' => attachment.data.bytesize.to_s,
-        #   'Content-Type' => attachment.file_type,
-        #   'Cache-Control' => "public, maxage=#{1.year.to_i}",
-        #   'Content-Disposition' => "inline; filename=\"#{attachment.file_name}\""
-        #   },
-        # [attachment.data]]
-        send_data attachment.data, :type => attachment.file_type, :disposition => 'inline'
+        [200, {
+          'Content-Length' => attachment.data.bytesize.to_s,
+          'Content-Type' => attachment.file_type,
+          'Cache-Control' => "public, maxage=#{1.year.to_i}",
+          'Content-Disposition' => "inline;filename=\"#{attachment.file_name}\""
+          },
+        [attachment.data]]
+        # send_data attachment.data, :type => attachment.file_type, :disposition => 'inline'
       else
         [404, {}, ["Attachment not found"]]
       end
