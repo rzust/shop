457e01b19ddbbe3fb1049425dcf2b0f35e0d976b
rzust <renatozust@gmail.com>
2014-12-11 23:21:28 -0500

    fix

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,2 +1,5 @@
 class ShopController < ApplicationController
+  def index
+    @featured = Shoppe::Product.where(featured: true).limit(8).includes(:default_image, :product_category, :variants).root.active.to_a
+  end
 end
